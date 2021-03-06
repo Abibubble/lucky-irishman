29143cea03aaa42263f427554aad8fa4cf733300
mihaelasandrea <mihaelasandrea@yahoo.com>
2021-03-06 01:46:25 +0000

    Add flip card javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,3 +36,98 @@ function copyrightYear() {
 }
 
 copyrightYear();
+
+// --------------------------------------------------------------------- flip cards
+
+class LuckyIrishman {
+    constructor(totalTime, cards) {
+        this.cardsArray = cards;
+        this.totalTime = totalTime;
+        this.timeRemaining = totalTime;
+        this.timer = document.getElementById("time-remaining");
+    }
+
+    startGame() {
+        this.timeRemaining = this.totalTime;
+        this.busy = true;
+        setTimeout(() => {
+            this.shuffleCards(this.cardsArray);
+            this.countdown = this.startCountdown();
+            this.busy = false;
+         }, 500);
+        this.hideCards();
+        this.timer.innerText = this.timeRemaining;
+    }
+
+    hideCards() {
+        this.cardsArray.forEach(card => {
+            card.classList.remove("visible");
+            card.classList.remove("matched");
+        });
+    }
+
+    flipCard(card) {
+        if(this.canFlipCard(card)) {
+            this.totalClicks++;
+            card.classList.add("visible");
+        }
+    }
+
+    getCardType(card) {
+        return card.getElementsByClassName("card-value")[0].src;
+    }
+
+    startCountdown() {
+        return setInterval(() => {
+            this.timeRemaining--;
+            this.timer.innerText = this.timeRemaining;
+            if(this.timeRemaining === 0)
+            this.gameOver();
+        }, 1000);
+    }
+
+    gameOver() {
+        clearInterval(this.countdown);
+        document.getElementById("game-over-text").classList.add("visible");
+        this.hideCards();
+    }
+
+    shuffleCards() {
+        for(let i = this.cardsArray.length - 1; i > 0; i--) {
+            let randIndex = Math.floor(Math.random() * (i+1));
+            this.cardsArray[randIndex].style.order = i;
+            this.cardsArray[i].style.order = randIndex;
+        }
+    }
+
+    canFlipCard(card) {
+        return !this.busy  && card !== this.cardToCheck;
+    }
+}
+
+// -------------------------------------------------------------------------------------------------readyState function
+
+if(document.readyState == "loading") {
+    document.addEventListener("DOMContentLoaded", ready());
+} else {
+    ready();
+}
+
+function ready(){
+    let overlays = Array.from(document.getElementsByClassName("overlay-text"));
+    let cards = Array.from(document.getElementsByClassName("card-flip"));
+    let game = new LuckyIrishman(120, cards);
+
+    overlays.forEach(overlay => {
+        overlay.addEventListener("click", () => {
+            overlay.classList.remove("visible");
+            game.startGame();
+        });
+    });
+
+    cards.forEach(card => {
+        card.addEventListener("click", () => {
+            game.flipCard(card);
+        });
+    });
+}
